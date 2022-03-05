e1b699eddb04e85d27bc8e7cabf19f21fa84bde8
Combei <>
2022-03-05 11:20:16 +0000

    loops

---
+++ basics/week2/loops/ex3.py
@@ -0,0 +1,6 @@
+answer = ""
+while answer.lower() != "stop":
+  print("Dormamu, i come to bargain!")
+  answer = input()
+
+print("You won, dr. Strange")
---
+++ basics/week2/loops/ex6.py
@@ -0,0 +1,9 @@
+n = int(input('Please enter a number'))
+while n > 0:
+   # check even and odd
+    if n % 2 == 0:
+      print(n, 'is a even number')
+    else:
+      print(n, 'is an odd number')
+    # decrese number by 1 in each interaction
+    n = n- 1
