ecb362dd5872541d7b120c19153fe18b4b35d32c
Combei <>
2022-03-10 19:35:13 +0000

    Replit

---
+++ basics/week2/loops/simple.py
@@ -0,0 +1,8 @@
+print("how many cable should i remove?")
+cable_to_remove = int(input())
+
+cable_remove = 0
+
+while (cable_remove < cable_to_remove):
+  print("removed cable")
+  cable_removed = cable_removed + 1
